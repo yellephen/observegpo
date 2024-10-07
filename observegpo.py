9f3c7f9e059d3efb04c5ee602997fbf969b4615e
yellephen <ds_walters@hotmail.com>
2024-10-07 15:04:20 -0400

    improvements to python version

--- observegpo.py
+++ observegpo.py
@@ -32,11 +32,19 @@ async def client(url,query,attributes):
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--constring', default="", help="The msldap connection string.")
-    #parser.add_argument('-u', '--user', default="", help="The last name to use in the  combination.")
+    parser.add_argument('-u', '--user', default="", help="Repeat the sAMAccountName of the user running the query")
     args = parser.parse_args()
     
+    query = f"(sAMAccountName={args.user})"
+    currentuser = await client(args.constring, query,["distinguishedName"])
+
+    #print(currentuser)
+    userDN = currentuser[0]["attributes"]["distinguishedName"]
+    print(f"Current user: {userDN}")
+    print("")
+
     query = "(objectclass=groupPolicyContainer)"
-    gpos = await client(args.constring, query,["name","displayname","gpcfilesyspath"])
+    gpos = await client(args.constring, query,["name","displayname","gpcfilesyspath","whenCreated"])
     #print(f"GPOs found:{len(gpos)}")
 
     ou_tasks = []
@@ -44,18 +52,25 @@ async def main():
         #print(gpo["attributes"]["displayName"])
         name = gpo["attributes"]["name"]
         query = f"(&(objectCategory=organizationalUnit)(gplink=*{name}*))"
-        ou_tasks.append(client(args.constring, query, attributes=["displayname","name","distinguishedname","whencreated"]))
+        ou_tasks.append(client(args.constring, query, attributes=["distinguishedname"]))
 
     ou_results = await asyncio.gather(*ou_tasks)
     backtrackindex = 0
     for ous in ou_results:
         print("===============================================")
-        print("GPO: " + gpos[backtrackindex]["attributes"]["displayName"])
+        print("GPO Display Name: " + gpos[backtrackindex]["attributes"]["displayName"])
         #print(f"OUs linked to GPO: {len(ous)}")
+        if (len(ous) == 0):
+            print("[ ] No linked ous")
         for ou in ous:
-            print("[] linked ou: " + ou["attributes"]["distinguishedName"])
             path = gpos[backtrackindex]["attributes"]["gPCFileSysPath"]
-            print(f"[] policy located at:{path}")
+            print(f"[x] policy located at: {path}")
+            whenCreated = gpos[backtrackindex]["attributes"]["whenCreated"]
+            print(f"[x] Policy created at: {whenCreated}")
+            print("[x] linked ou: " + ou["attributes"]["distinguishedName"])
+            
+            
+            
         backtrackindex += 1
 
 if __name__ == "__main__":
