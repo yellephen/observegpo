2f89cbcfc3223cde15b954f0ecb8606fc9463c1e
yellephen <ds_walters@hotmail.com>
2024-10-09 17:03:40 -0400

    added file crawl

--- observegpo.py
+++ observegpo.py
@@ -1,6 +1,20 @@
 import asyncio
 from msldap.commons.factory import LDAPConnectionFactory
 import argparse
+import os
+from smbclient import listdir
+from smbclient.path import isdir
+import smbclient
+
+def list_files_recursively(directory, username, password):
+    files = []
+    for dir_entry in smbclient.listdir(directory, username=username, password=password):
+        if not isdir(directory+"\\"+dir_entry):
+            files.append(directory+"\\"+dir_entry)
+        if isdir(directory+"\\"+dir_entry):
+            files.extend(list_files_recursively(directory+"\\"+dir_entry,username,password))           
+    return files
+            
 
 def is_user_in_ou(user_dn, ou_dn):
     user_dn_parts = user_dn.split(',')
@@ -35,20 +49,31 @@ async def client(url,query,attributes):
         
         entries.append(entry)
 
+    await ldap_client.disconnect()
     return entries
 
-    
-    
+def ProcessSysVol(path, username, password):
+    files = list_files_recursively(path, username, password)
+    if len(files) == 0:
+        print("[ ] No files found in SYSVOL for this policy.")
+    else:
+        print("[x] Paths to files in SYSVOL")
+        for file in files:
+            print(file)
 
 #$searcher.Filter = "(&(objectCategory=organizationalUnit)(gplink=*$($name)*))"
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--constring', default="", help="The msldap connection string.")
-    parser.add_argument('-u', '--user', default="", help="Repeat the sAMAccountName of the user running the query")
+    parser.add_argument('-q', '--queryuser', default="", help="Repeat the sAMAccountName of the user running the query.")
+    parser.add_argument('-t','--dcip',help="IP of a DC holding the SYSVOL to inspect.")
+    parser.add_argument('-u','--authuser',help="Username to connect to SYSVOL with")
+    parser.add_argument('-p','--authpassword',help="Password to connect to SYSVOL with")
+    
     args = parser.parse_args()
     
-    query = f"(sAMAccountName={args.user})"
+    query = f"(sAMAccountName={args.queryuser})"
     currentuser = await client(args.constring, query,["distinguishedName"])
 
     #print(currentuser)
@@ -60,32 +85,32 @@ async def main():
     gpos = await client(args.constring, query,["name","displayname","gpcfilesyspath","whenCreated"])
     #print(f"GPOs found:{len(gpos)}")
 
-    ou_tasks = []
+   
     for gpo in gpos:
         #print(gpo["attributes"]["displayName"])
-        name = gpo["attributes"]["name"]
-        query = f"(&(objectCategory=organizationalUnit)(gplink=*{name}*))"
-        ou_tasks.append(client(args.constring, query, attributes=["distinguishedname"]))
-
-    ou_results = await asyncio.gather(*ou_tasks)
-    backtrackindex = 0
-    for ous in ou_results:
+        name = gpo["attributes"]["name"]     
         print("===============================================")
-        print("GPO Display Name: " + gpos[backtrackindex]["attributes"]["displayName"])
-        #print(f"OUs linked to GPO: {len(ous)}")
-        if (len(ous) == 0):
+        print("GPO Display Name: " + gpo["attributes"]["displayName"])
+        path = gpo["attributes"]["gPCFileSysPath"]
+        print(f"[x] policy located at: {path}")
+        whenCreated = gpo["attributes"]["whenCreated"]
+        print(f"[x] Policy created at: {whenCreated}")
+        
+        query = f"(&(objectCategory=organizationalUnit)(gplink=*{name}*))" 
+        ou_results = await client(args.constring, query, attributes=["distinguishedname"])
+        if (len(ou_results) == 0):
             print("[ ] No linked ous")
-        for ou in ous:
-            path = gpos[backtrackindex]["attributes"]["gPCFileSysPath"]
-            print(f"[x] policy located at: {path}")
-            whenCreated = gpos[backtrackindex]["attributes"]["whenCreated"]
-            print(f"[x] Policy created at: {whenCreated}")
+        for ou in ou_results:
             print("[x] linked ou: " + ou["attributes"]["distinguishedName"])
             if is_user_in_ou(userDN,ou["attributes"]["distinguishedName"]):
                 print("[x] user in this ou or a decendant")
-            
-            
-        backtrackindex += 1
+        
+        parts = path.split('\\')
+        parts[2] = args.dcip
+        ippath = '\\'.join(parts)
 
+        
+        ProcessSysVol(ippath,args.authuser,args.authpassword)
+    
 if __name__ == "__main__":
     asyncio.run(main())
