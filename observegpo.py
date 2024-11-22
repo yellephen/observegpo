4b2587d1fac9fa1ec1bb77b1e1fef7e7052e5317
yellephen <ds_walters@hotmail.com>
2024-11-22 11:25:39 -0500

    fixed bug where all files shown as writable

--- observegpo.py
+++ observegpo.py
@@ -53,6 +53,19 @@ async def client(url,query,attributes):
     await ldap_client.disconnect()
     return entries
 
+async def get_acl(url,dn):
+    conn_url = LDAPConnectionFactory.from_url(url)
+    ldap_client = conn_url.get_client()
+    _, err = await ldap_client.connect()
+    if err is not None:
+        raise err
+  
+    entries = []
+    entry = await ldap_client.get_objectacl_by_dn(dn)
+    
+    await ldap_client.disconnect()
+    return entry
+
 def ProcessSysVol(path, username, password):
     files = list_files_recursively(path, username, password)
     if len(files) == 0:
@@ -61,9 +74,12 @@ def ProcessSysVol(path, username, password):
         print("[x] Paths to files in SYSVOL")
         for file in files:
             print(file)
-            with smbclient.open_file(file, 'rb') as fd:
-                if fd.writable:
+            try:
+                with smbclient.open_file(file,'w') as fdw:
                     print("     [x] The above file is writable by the smbuser passed.")
+            except:
+                print("     [ ] No write access to above file.")
+            with smbclient.open_file(file, 'rb') as fd:
                 if file.split(".")[-1] == "pol":
                     # Iterate over each registry value and print its details
                     print("Policy file found, dumping kvps.")
@@ -85,7 +101,7 @@ def ProcessSysVol(path, username, password):
                             data = int.from_bytes(value.data,'little')
                             print(f"Key:{value.key}\{value.value} Value:{data} Type:REG_DWORD")        
                         elif value.type == 5:
-                            data = value.data
+                            data = int.from_bytes(value.data,'big')
                             print(f"Key:{value.key}\{value.value} Value:{data} Type:REG_DWORD_BIG_ENDIAN")   
                         elif value.type == 6:
                             data = value.data
@@ -97,9 +113,12 @@ def ProcessSysVol(path, username, password):
                             data = value.data
                             print(f"Key:{value.key}\{value.value} Value:{data} Type:REG_RESOURCE_LIST")   
                         elif value.type == 11:
-                            data = value.data
+                            data = int.from_bytes(value.data,'little')
                             print(f"Key:{value.key}\{value.value} Value:{data} Type:REG_QWORD")                                                                                                                                                                                                                   
-                
+
+
+
+
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--constring', default="", required=True, help="The msldap connection string. eg. ldap+ntlm-password://TEST\\victim:<password>@10.10.10.2")
@@ -120,12 +139,12 @@ async def main():
         print("")
 
     query = "(objectclass=groupPolicyContainer)"
-    gpos = await client(args.constring, query,["name","displayname","gpcfilesyspath","whenCreated"])
+    gpos = await client(args.constring, query,["name","displayname","gpcfilesyspath","whenCreated","distinguishedName"])
     #print(f"GPOs found:{len(gpos)}")
 
    
     for gpo in gpos:
-        #print(gpo["attributes"]["displayName"])
+        
         name = gpo["attributes"]["name"]     
         print("===============================================")
         print("GPO Display Name: " + gpo["attributes"]["displayName"])
